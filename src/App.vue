84e831b6af2874a21437f4479f772d094d77c0dd
David Susco <dsusco@clarku.edu>
2023-05-18 10:35:00 -0400

    added version to footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
+const version = appVersion
 </script>
 
 <template>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import fs from 'fs'
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const packageJSON = JSON.parse(fs.readFileSync('./package.json'))
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +12,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  define: {
+    appVersion: JSON.stringify(packageJSON.version)
   }
 })

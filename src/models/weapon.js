8f70119738f0fb02084529f7c0aa8455225e820c
David Susco <dsusco@clarku.edu>
2023-05-31 15:12:53 -0400

    enforcing strings in model contructors

--- src/models/weapon.js
+++ src/models/weapon.js
@@ -2,8 +2,8 @@ import WEAPON_TYPES from '@/constants/weapon-types'
 
 export default class Weapon {
   constructor (name = '', type = Object.keys(WEAPON_TYPES)[0]) {
-    this.name = name
-    this.type = type
+    this.name = `${name}`
+    this.type = `${type}`
     this.rangeBands = []
     this.eccentricities = []
   }

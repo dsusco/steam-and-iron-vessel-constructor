9a71f133c38eed991bc33f01887b5deef4685785
David Susco <dsusco@clarku.edu>
2023-07-27 14:34:23 -0400

    added dist

--- src/helpers/eccentricity-togglers/index.js
+++
@@ -1,3 +0,0 @@
-export { default as prohibitedEccentricities } from './prohibited-eccentricities'
-export { default as requiredEccentricities } from './required-eccentricities'
-export { default as requiredTypes } from './required-types'
--- src/models/battery.js
+++
@@ -1,5 +0,0 @@
-export default class Battery {
-  constructor () {
-    this.weaponId = ''
-  }
-}
